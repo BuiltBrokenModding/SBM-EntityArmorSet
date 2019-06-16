24b98811743f9f96a6469470cc785a7dc0800f26
Robert WC Seifert <rseifert.phone@gmail.com>
2019-06-15 23:12:09 -0400

    Fixed hands not being set

--- src/main/java/com/builtbroken/armorset/EntityArmorSetter.java
+++ src/main/java/com/builtbroken/armorset/EntityArmorSetter.java
@@ -1,6 +1,5 @@
 package com.builtbroken.armorset;
 
-import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
@@ -32,54 +31,57 @@ public class EntityArmorSetter
         if (!event.getWorld().isRemote && event.getEntityPlayer().isCreative())
         {
             final ItemStack stack = event.getItemStack();
-
-            //Mod support, allow item to define its insert slot
-            EntityEquipmentSlot slot = stack.getItem().getEquipmentSlot(stack);
-
-            //Default logic
-            if (slot == null)
+            if(!stack.isEmpty())
             {
-                if (stack.getItem() != Item.getItemFromBlock(Blocks.PUMPKIN) && stack.getItem() != Items.SKULL)
+
+                //Mod support, allow item to define its insert slot
+                EntityEquipmentSlot slot = stack.getItem().getEquipmentSlot(stack);
+
+                //Default logic
+                if (slot == null)
                 {
-                    if (stack.getItem() instanceof ItemArmor)
+                    if (stack.getItem() != Item.getItemFromBlock(Blocks.PUMPKIN) && stack.getItem() != Items.SKULL)
                     {
-                        slot = ((ItemArmor) stack.getItem()).armorType;
-                    }
-                    else if (stack.getItem() == Items.ELYTRA)
-                    {
-                        slot = EntityEquipmentSlot.CHEST;
-                    }
-                    //Shields are forced to offhand
-                    else if (stack.getItem().isShield(stack, null))
-                    {
-                        slot = EntityEquipmentSlot.OFFHAND;
-                    }
-                    //Allow inverting hand with sneak
-                    else if (event.getEntityPlayer().isSneaking())
-                    {
-                        event.getTarget().setItemStackToSlot(EntityEquipmentSlot.OFFHAND, stack.copy());
+                        if (stack.getItem() instanceof ItemArmor)
+                        {
+                            slot = ((ItemArmor) stack.getItem()).armorType;
+                        }
+                        else if (stack.getItem() == Items.ELYTRA)
+                        {
+                            slot = EntityEquipmentSlot.CHEST;
+                        }
+                        //Shields are forced to offhand
+                        else if (stack.getItem().isShield(stack, null))
+                        {
+                            slot = EntityEquipmentSlot.OFFHAND;
+                        }
+                        //Allow inverting hand with sneak
+                        else if (event.getEntityPlayer().isSneaking())
+                        {
+                            slot = EntityEquipmentSlot.OFFHAND;
+                        }
+                        else
+                        {
+                            slot = EntityEquipmentSlot.MAINHAND;
+                        }
+                        //TODO add ctrl to force blocks into head slot
                     }
                     else
                     {
-                        event.getTarget().setItemStackToSlot(EntityEquipmentSlot.MAINHAND, stack.copy());
+                        slot = EntityEquipmentSlot.HEAD;
                     }
-                    //TODO add ctrl to force blocks into head slot
                 }
-                else
+
+
+                //Insert
+                if (slot != null)
                 {
-                    slot = EntityEquipmentSlot.HEAD;
+                    event.getTarget().setItemStackToSlot(slot, stack.copy());
                 }
+
+                event.setCanceled(true);
+                event.setCancellationResult(EnumActionResult.SUCCESS);
             }
-
-
-            //Insert
-            if (slot != null)
-            {
-                event.getTarget().setItemStackToSlot(slot, stack.copy());
-            }
-
-            event.setCanceled(true);
-            event.setCancellationResult(EnumActionResult.SUCCESS);
         }
     }
 }
